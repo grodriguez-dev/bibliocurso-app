cdf20df9a1870cc60e6f46f9d276b17ddcf7db8c
[grodriguez-dev] <[grodriguez.dev99@gmail.com]>
2021-06-23 12:28:05 -0400

    agregado cambios

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -1,15 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Administrador</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('Layouts.app')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
+@section('content')
     <nav class="navbar navbar-expand navbar-light bg-light">
         <div class="nav navbar-nav">
             <img width="40" src="{{asset('gr-logo.png')}}" />
@@ -61,5 +52,4 @@
         </div>
     </div>
     <br>
-  </body>
-</html>
+@endsection
